eddecf71d5e22a95d70d90f195d42215e9ffdbed
SyedSaifHossain <syedsaifnibir@gmail.com>
2026-04-03 15:56:46 +0600

    Design main screen

--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -16,22 +16,75 @@ class main_screen extends StatelessWidget {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 50,right: 20,left: 20),
-            child: Column(
-             children: [
-               Text(
-                 "SLH(Sydney Luxury Hire)",
-                 style: TextStyle(
-                   fontSize: 20,
-                   fontStyle: FontStyle.italic,
-                   fontWeight: FontWeight.bold,
-                   fontFamily: "Poppins",
-                   color: Color(0xFF000000),
+          Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 70),
+                alignment: Alignment.center,
+                child: Image(image: AssetImage("assets/images/slhlogo.png")),
+              ),
+              const SizedBox(height: 35),
+              Container(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    Text(
+                      "SLH(Sydney Luxury Hire)",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontStyle: FontStyle.italic,
+                        fontFamily: "Poppins",
+                        color: Color(0xFF000000),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Welcome to our car rental service!.",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Poppins",
+                        color: Color(0xFF000000),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+               const SizedBox(height: 50),
+               Container(
+                 height: 300,
+                 child: Stack(
+                   children: [
+                     Transform.translate(
+                       offset: Offset(MediaQuery.of(context).size.width * -0.20, 0),
+                       child: Image(
+                         image: AssetImage("assets/images/frontcarimg.png"),
+                         height: 400,
+                         width: MediaQuery.of(context).size.width,
+                         fit: BoxFit.contain,
+                       ),
+                     ),
+                   ],
                  ),
                ),
-             ],
-            ),
+                const SizedBox(height: 50),
+
+              ElevatedButton(
+                onPressed: () {
+                },
+                style:ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF000000).withOpacity(0.7),
+                  foregroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: Text("Get Started"),
+              ),
+
+            ],
           ),
         ],
       ),
